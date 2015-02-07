277423c206d27b6376723de25d39bcd137882f17
tfull <tfull154008@gmail.com>
2015-02-07 09:28:46 +0900

    add

---
+++ back_propagation/Main.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+import sys
+import random as R
+
+import NeuralNetwork as NN
+
+TRAIN_N = 10000
+DATA_N = 10000
+
+def main(ws):
+    n = len(ws)
+    train_l = []
+    data_l = []
+
+    for t in range(TRAIN_N):
+        xs = []
+        for i in range(n):
+            xs.append(random.randint(0, 200) / 10 - 10)
+        train_l.append((xs, [function(ws, xs)]))
+
+    for t in range(DATA_N):
+        xs = []
+        for i in range(n):
+            xs.append(random.randint(0, 200) / 10 - 10)
+        data_l.append((xs, [function(ws, xs)]))
+
+    perceptron = P.Perceptron(n, 8, 1, 0.02)
+    perceptron.train(train_l)
+
+    count = 0
+
+    for data in data_l:
+        (xs, t) = data
+        result = perceptron.execute(xs)
+        if result == t:
+            count += 1
+
+    print(count / DATA_N)
+
+def function(ws, xs):
+    return sign_p(ws[0] + sum([w * x for (w, x) in zip(ws[1:], xs)]))
+
+def sign_p(x):
+    if x >= 0:
+        return 1
+    else:
+        return -1
+
+if __name__ == '__main__':
+    args = sys.argv
+    main([float(w) for w in args[1:]])
---
+++ back_propagation/NeuralNetwork.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+import math
+import random as R
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(- x))
+
+class NeuralNetWork:
+    def __init__(self, s_n, a_n, r_n, theta, eta):
+        self.s_n = s_n
+        self.a_n = a_n
+        self.r_n = r_n
+        self.theta = theta
+        self.eta = eta
+        self.sa_m = [[R.random() for j in range(a_n)] for i in range(s_n)]
+        self.ar_m = [[R.random() for j in range(r_n)] for i in range(a_n)]
+
+    def function(self, ws, xs):
+        return sigmoid(sum([w * x for (w, x) in zip(ws, xs)]) - self.theta)
+
+    def train(self, train_l):
+
+        def delta(ts, os, k):
+            return (ts[k] - os[k]) * os[k] * (1 - os[k])
+
+        def 
+
+        for tr in train_l:
+            (xs, ts) = tr
+            (os, hs) = self.connect(xs)
+
+            for r_i in range(self.r_n):
+                for a_i in range(self.a_n):
+                    self.ar_m[a_i][r_i] += self.eta * delta(ts, os, r_i) * self.hs[a_i]
+
+            for a_i in range(self.a_n):
+                for s_i in range(self.s_n):
+                    self.sa_m[s_i][a_i] += self.eta * hs[a_i] * (1 - hs[a_i]) * sum([self.ar_m[a_i][r_i] * delta(ts, os, r_i) for r_i in range(self.r_n)]) * xs[s_i]
+
+    def connect(self, xs):
+        hs = [0 for i in range(self.a_n)]
+        os = [0 for i in range()]
+
+        for a_i in range(self.a_n):
+            hs[a_i] = self.function([self.sa_m[i][a_i] for i in range(self.s_n)], xs)
+        for r_i in range(self.r_n):
+            os[r_i] = self.function([self.ar_m[i][r_i] for i in range(self.a_n)], hs)
+
+        return (os, hs)
